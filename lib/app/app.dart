7595ac2a023138cdfe1359058b608c4298695285
kume1a <kumela011@gmail.com>
2025-06-01 18:44:55 +0400

    add welcome page

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:lexia/pages/welcome_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Lexia', debugShowCheckedModeBanner: false);
+    return MaterialApp(title: 'Lexia', debugShowCheckedModeBanner: false, home: WelcomePage());
   }
 }
