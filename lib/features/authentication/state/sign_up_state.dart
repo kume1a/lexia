53f37799394389b62372d06e45b65c95b07db470
kume1a <kumela011@gmail.com>
2025-06-02 23:00:03 +0400

    signup in progress

---
+++ lib/features/authentication/state/sign_up_state.dart
@@ -0,0 +1,114 @@
+import 'package:common_models/common_models.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../../app/configuration/app_environment.dart';
+import '../../../app/navigation/page_navigator.dart';
+
+part 'sign_up_state.freezed.dart';
+
+@freezed
+class SignUpState with _$SignUpState {
+  const factory SignUpState({
+    required Name username,
+    required Email email,
+    required Password password,
+    required RepeatedPassword repeatedPassword,
+    required bool isSubmitting,
+    required bool validateForm,
+  }) = _SignUpState;
+
+  factory SignUpState.initial() => SignUpState(
+    username: Name.empty(),
+    email: Email.empty(),
+    password: Password.empty(),
+    repeatedPassword: RepeatedPassword.empty(),
+    isSubmitting: false,
+    validateForm: false,
+  );
+}
+
+extension SignUpCubitX on BuildContext {
+  SignUpCubit get signUpCubit => read<SignUpCubit>();
+}
+
+@injectable
+class SignUpCubit extends Cubit<SignUpState> {
+  SignUpCubit(this._pageNavigator) : super(SignUpState.initial());
+
+  final PageNavigator _pageNavigator;
+
+  final usernameFieldController = TextEditingController();
+  final emailFieldController = TextEditingController();
+  final passwordFieldController = TextEditingController();
+  final repeatedPasswordFieldController = TextEditingController();
+
+  String _repeatedPasswordValue = '';
+
+  void onEmailChanged(String email) {
+    emit(state.copyWith(email: Email(email)));
+  }
+
+  void onPasswordChanged(String password) => emit(
+    state.copyWith(
+      password: Password(password),
+      repeatedPassword: RepeatedPassword(password, _repeatedPasswordValue),
+    ),
+  );
+
+  void onRepeatedPasswordChanged(String repeatedPassword) {
+    _repeatedPasswordValue = repeatedPassword;
+    emit(state.copyWith(repeatedPassword: RepeatedPassword(state.password.get ?? '', repeatedPassword)));
+  }
+
+  Future<void> onSubmit() async {
+    emit(state.copyWith(validateForm: true));
+
+    if (state.email.invalid ||
+        state.username.invalid ||
+        state.password.invalid ||
+        state.repeatedPassword.invalid) {
+      return;
+    }
+
+    // emit(state.copyWith(isSubmitting: true));
+    // final result = await _authService.signUp(
+    //   username: state.username.getOrThrow,
+    //   email: state.email.getOrThrow,
+    //   password: state.password.getOrThrow,
+    // );
+    // emit(state.copyWith(isSubmitting: false));
+
+    // result.fold(_failureNotifierPool.signUp.notify, (_) async {
+    //   await _afterSignUp();
+
+    //   _pageNavigator.toConfirmAccountEmailPage();
+    // });
+  }
+
+  Future<void> onDevSignIn() async {
+    final username = AppEnvironment.devSignInUsername;
+    final email = AppEnvironment.devSignInEmail;
+    final password = AppEnvironment.devSignInPassword;
+
+    emit(
+      state.copyWith(
+        username: Name(username),
+        email: Email(email),
+        password: Password(password),
+        repeatedPassword: RepeatedPassword(password, password),
+      ),
+    );
+
+    emailFieldController.text = email;
+    passwordFieldController.text = password;
+
+    return onSubmit();
+  }
+
+  void onSignInPressed() {
+    _pageNavigator.pop();
+  }
+}
