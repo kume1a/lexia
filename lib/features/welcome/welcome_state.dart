89eabede9e56c7f01b280fbf1e21fb5a23cc5216
kume1a <kumela011@gmail.com>
2025-06-03 16:09:48 +0400

    feat: refactor WelcomeCubit and WelcomePage to use Freezed for state management and improve UI responsiveness

--- lib/features/welcome/welcome_state.dart
+++ lib/features/welcome/welcome_state.dart
@@ -1,18 +1,28 @@
 import 'package:common_models/common_models.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../app/navigation/page_navigator.dart';
 import '../authentication/api/auth_status_provider.dart';
+
+part 'welcome_state.freezed.dart';
+
+@freezed
+class WelcomeState with _$WelcomeState {
+  const factory WelcomeState({required SimpleDataState<bool> isAuthenticatedState}) = _WelcomeState;
+
+  factory WelcomeState.initial() => WelcomeState(isAuthenticatedState: SimpleDataState.idle());
+}
 
 extension WelcomeCubitExtension on BuildContext {
   WelcomeCubit get welcomeCubit => read<WelcomeCubit>();
 }
 
 @injectable
-class WelcomeCubit extends Cubit<Unit> {
-  WelcomeCubit(this._authStatusProvider, this._pageNavigator) : super(unit) {
+class WelcomeCubit extends Cubit<WelcomeState> {
+  WelcomeCubit(this._authStatusProvider, this._pageNavigator) : super(WelcomeState.initial()) {
     _init();
   }
 
@@ -20,7 +30,11 @@ class WelcomeCubit extends Cubit<Unit> {
   final PageNavigator _pageNavigator;
 
   Future<void> _init() async {
+    emit(state.copyWith(isAuthenticatedState: SimpleDataState.loading()));
+
     final isAuthenticated = await _authStatusProvider.get();
+
+    emit(state.copyWith(isAuthenticatedState: SimpleDataState.success(isAuthenticated)));
 
     if (isAuthenticated) {
       _pageNavigator.toMain();
